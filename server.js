f4cab3d7e151fc4bca63616d298542d8b1ca880e
Amir <amirhatam136@gmail.com>
2022-01-18 17:58:42 +0100

    add new funtionality to connect db

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ require('dotenv').config();
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-const { debug } = require("./middlewares/debug")
+// const { debug } = require("./middlewares/debug")
 const router = express.Router()
 
 // const { port, mongoURL } = require('./utils/config')
 
 const authRoutes = require("./routes/authRoutes")
 const usersRoutes = require("./routes/userRoutes")
-const { PORT, MONGODB_URI } = process.env
+// const { PORT, MONGODB_URI } = process.env
 
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/azimutoDB', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
         console.error(err);
     } else {
@@ -24,25 +25,29 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static("./public"))
-app.use(debug)
+// app.use(debug)
 
 
-const port = PORT || 8080
+// const port = PORT || 8080
+const port = process.env.PORT || 8080
 
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, './public'), function (err) {
-        if (err) {
-            res.status(500).send(err)
-        }
-    })
-})
+
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, './public'), function (err) {
+//         if (err) {
+//             res.status(500).send(err)
+//         }
+//     })
+// })
 
 app.use("/", router)
 app.use("/user", authRoutes)
 app.use("/users", usersRoutes)
 
-
+if (process.env.NODE_ENV === 'production') {
+    console.log("NODE_ENV :", process.env.NODE_ENV);
+}
 
 app.listen(port, () => {
     console.log("Server is listening at port ", port);
